f62ed71864e9e9ec18016d859a51eff529437cfc
Viraj Abeywardana <abeywardana21@gmail.com>
2024-02-26 12:21:38 +0530

    create and start server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,21 @@
-import { Express } from "express";
+import express from "express";
 import * as dotenv from "dotenv";
 import cors from 'cors';
+import connectDB from "./Mongodb/connect";
+
+//this helps to pull env variables from the .env file
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.get('/', async(req,res)=>{
+    res.send('Hello From DALL-E');
+});
+
+const startServer = async() => {
+    app.listen(8080, () => console.log('Server started on port http://localhost:8080'));
+}
+
+startServer();
