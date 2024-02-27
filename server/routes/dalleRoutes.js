36765a1d88bd79a7729c7834b33b3c41a4fb537d
Viraj Abeywardana <abeywardana21@gmail.com>
2024-02-27 13:33:27 +0530

    calling dalle api and get image

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,8 +1,35 @@
-import { Express } from "express";
+import express from "express";
 import * as dotenv from "dotenv";
-import {Configuration, OpenAIApi} from "openai";
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  });
+
+
+router.route("/").get((req, res) => {
+    res.send("Hello from DALL-E!");
+});
+
+router.route("/").post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+        const aiResponse = await openai.createImage({
+            pormpt,
+            n:1,
+            size:"1024x1024",
+            response_format:"b64_json"
+        });
+        const image = aiResponse.data.data[0].b64_json;
+        res.status(200).json({ photo: image });
+    }catch(error){
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+export default router;
